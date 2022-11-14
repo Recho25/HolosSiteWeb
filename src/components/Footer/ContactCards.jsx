08b135ddc4a978c61f7ddce98f7369c8cd26f7ed
Étienne Lachance-Perreault <etienne.lach@gmail.com>
2022-11-14 12:13:49 -0500

    need more change

--- src/components/Footer/ContactCards.jsx
+++ src/components/Footer/ContactCards.jsx
@@ -10,38 +10,31 @@ function GroupExample() {
                     <Card.Img variant="top" src="/img/sfl_expertise.png" style={{marginLeft:"1em"}} />
                 </a>
                 <Card.Body>
-                    {/*<Card.Title></Card.Title>*/}
                     <Card.Text>
                         SFL Expertise, c’est un centre d’expertise en pleine croissance dont les outils d’analyse distinctifs reposent sur l’innovation et la recherche-développement. C’est une équipe de professionnels qui travaillent conjointement avec votre conseiller en sécurité financière pour vous aider à prendre des décisions financières éclairées qui vous permettront de réaliser vos projets d’avenir.
                     </Card.Text>
                 </Card.Body>
-                {/*<Card.Footer>*/}
-                {/*    <small className="text-muted">Expertise</small>*/}
-                {/*</Card.Footer>*/}
             </Card>
             <Card>
-                <Card.Img variant="top" src="/img/sfl_patrimoine.jpg" />
+                <a href={"https://www.sfl.ca/"}>
+                    <Card.Img variant="top" src="/img/sfl_patrimoine.jpg" />
+                </a>
                 <Card.Body>
-                    {/*<Card.Title>Card title</Card.Title>*/}
                     <Card.Text>
                         Peu importe la complexité de votre situation financière, vous souhaitez réaliser vos rêves et vos projets de vie les plus chers. Votre conseiller SFL vous offre ses conseils avisés pour les réaliser tout en protégeant votre bien-être et celui de vos proches.
                     </Card.Text>
                 </Card.Body>
-                {/*<Card.Footer>*/}
-                {/*    <small className="text-muted">Fiabilite</small>*/}
-                {/*</Card.Footer>*/}
+
             </Card>
             <Card>
-                <Card.Img variant="top" src="/img/planipret.webp" style={{marginLeft:"1em"}}/>
+                <a href={"https://www.planipret.com/fr/"}>
+                    <Card.Img variant="top" src="/img/planipret.webp" style={{marginLeft:"1em"}}/>
+                </a>
                 <Card.Body>
-                    {/*<Card.Title>Card title</Card.Title>*/}
                     <Card.Text>
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
+                        Que ce soit pour un refinancement ou l’achat d’une propriété, Planiprêt négociera et vous aidera à comprendre votre hypothèque.
                     </Card.Text>
                 </Card.Body>
-                {/*<Card.Footer>*/}
-                {/*    <small className="text-muted">Honneur</small>*/}
-                {/*</Card.Footer>*/}
             </Card>
         </CardGroup>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import "./header.scss";
 import React, {Component, Fragment} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import StockRow from '../StockRow'
 
 
 class Header extends Component {
@@ -90,7 +89,7 @@ class Header extends Component {
                                 {this.pages}
                             </ul>
                             <div className="portfolio_link">
-                                <a href="https://id.desjardins.com/login?state=hKFo2SBpTGxnNmlkZTZ1TzhIa3JUMFZyYUpKaC1oTkpXcWVwVaFupWxvZ2luo3RpZNkgTERBZVQ4Um15U3pLejhHUnZQa0RhZjE0TF9PMG9fUG-jY2lk2SBsN0VxVnNvdXJlSEE0ZkpZQzgxcDJGN3VIaWFTa0FvRQ&client=l7EqVsoureHA4fJYC81p2F7uHiaSkAoE&protocol=oauth2&response_type=code&redirect_uri=https%3A%2F%2Fbrendfs.dataphile.broadridge.com%2Fcgi-bin%2Fwspdfsen.sh%2Fiapr%2Fexternal_auth.p&scope=openid&audience=pise-dsf&connection=gpap"><span>Portfolio</span></a>
+                                <a href="https://id.desjardins.com/login?state=hKFo2SBpTGxnNmlkZTZ1TzhIa3JUMFZyYUpKaC1oTkpXcWVwVaFupWxvZ2luo3RpZNkgTERBZVQ4Um15U3pLejhHUnZQa0RhZjE0TF9PMG9fUG-jY2lk2SBsN0VxVnNvdXJlSEE0ZkpZQzgxcDJGN3VIaWFTa0FvRQ&client=l7EqVsoureHA4fJYC81p2F7uHiaSkAoE&protocol=oauth2&response_type=code&redirect_uri=https%3A%2F%2Fbrendfs.dataphile.broadridge.com%2Fcgi-bin%2Fwspdfsen.sh%2Fiapr%2Fexternal_auth.p&scope=openid&audience=pise-dsf&connection=gpap"><span>Accédez à votre portefeuil</span></a>
                             </div>
                             <div onClick={this.toggleClass} className={"burger_btn_container"}>
                                 <div className={"btn_burger"}>
@@ -101,21 +100,21 @@ class Header extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className={"addons_info " + this.isTop()}>
-                        <div className="container">
-                            <div className="col-md-4 offset-md-4">
-                                <div className="card">
-                                    <ul className="list-group list-group-flush list-group-horizontal text-center">
-                                        <StockRow ticker="AAPL" />
-                                        <StockRow ticker="GOOG" />
-                                        <StockRow ticker="MSFT" />
-                                        <StockRow ticker="TSLA" />
-                                        <StockRow ticker="AMZN"/>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    {/*<div className={"addons_info " + this.isTop()}>*/}
+                    {/*    <div className="container">*/}
+                    {/*        <div className="col-md-4 offset-md-4">*/}
+                    {/*            <div className="card">*/}
+                    {/*                <ul className="list-group list-group-flush list-group-horizontal text-center">*/}
+                    {/*                    <StockRow ticker="AAPL" />*/}
+                    {/*                    <StockRow ticker="GOOG" />*/}
+                    {/*                    <StockRow ticker="MSFT" />*/}
+                    {/*                    <StockRow ticker="TSLA" />*/}
+                    {/*                    <StockRow ticker="AMZN"/>*/}
+                    {/*                </ul>*/}
+                    {/*            </div>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </header>
                 <div className={"nav_container " + this.giveClass()}>
                     <ul>
